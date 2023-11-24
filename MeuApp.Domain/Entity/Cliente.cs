e9756edb1678dcdcb3143e8a81057d652434bc10
caique  coelho pinheiro <caiquepinheiro69@gmai.com>
2023-11-23 23:45:11 -0300

    Criação das propriedas da classe Cliente e Entity
    Nesse commit verifica a criaçaõ das variaveis e seus tipos da classe Cliente e Entity

--- MeuApp.Domain/Entity/Cliente.cs
+++ MeuApp.Domain/Entity/Cliente.cs
@@ -1,32 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using System.Threading.Channels;
-using System.Threading.Tasks;
+﻿using MeuApp.Domain.Entity.Base;
 
 namespace MeuApp.Domain.Entity
 {
-    internal class Cliente
+    public class Cliente : EntityBase
     {
-        public int Id { get; set; }
-
         public string NomeCompleto { get; set; }
-
         public int NumeroDaCasa { get; set; }
-
         public int Idade { get; set; }
-
         public string Casado { get; set; }
-
-        public Cliente(int id, string nomeCompleto, int numeroDaCasa, int idade, string casado)
-        {
-            Id = id;
-            NomeCompleto = nomeCompleto;
-            NumeroDaCasa = numeroDaCasa;
-            Idade = idade;
-            Casado = casado;
-        }
 
         public void MaiorDeIdade(int idade)
         {
@@ -41,16 +22,12 @@ namespace MeuApp.Domain.Entity
                 default:
                     Console.WriteLine("Idade minima não alcançada");
                     break;
-
             }
-
         }
+
         public override string ToString()
         {
             return Id + NomeCompleto + Idade + Casado + NumeroDaCasa;
         }
-
     }
-
 }
-
