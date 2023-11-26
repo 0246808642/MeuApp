01ba22d4869d5cad592d99b8b973700057ff061d
caique  coelho pinheiro <caiquepinheiro69@gmai.com>
2023-11-26 13:10:00 -0300

    Desenvolvi a classe Interface e a Classe Enum

---
+++ MeuApp.Domain/Enum/Situations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MeuApp.Domain.Enum
+{
+    public enum Situations
+    {
+        Active = 1,
+        Inacitve,
+        Deleted
+    }
+}
---
+++ MeuApp.Domain/Interfaces/Base/ClienteRepository.cs
@@ -0,0 +1,11 @@
+﻿using MeuApp.Domain.Entity;
+using MeuApp.Domain.Enum;
+using MeuApp.Domain.Interfaces.Base;
+
+namespace MeuApp.Domain.Interfaces.Base
+{
+    public interface ClienteRepository : RepositoryBase<Cliente>
+    {
+        IQueryable<Cliente> GetAllActive(Situations pSituations);
+    }
+}
